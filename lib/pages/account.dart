8591ab65f00cc81df4f45082146c8b096dfa9180
blaszkaaa <58654924+blaszkaaa@users.noreply.github.com>
2022-10-04 12:10:27 +0200

    1

---
+++ lib/pages/account.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class accountUser extends StatelessWidget {
+  const accountUser({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
