b1c240e10917727ea903badfbb932be406f7e1a4
blaszkaaa <58654924+blaszkaaa@users.noreply.github.com>
2022-10-04 12:41:03 +0200

    1

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -8,8 +8,10 @@ class homeUser extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Center(
-          child: Text("Crypto Price"),
+        title: Center(child: Text("Crypto Price")),
+        leading: Padding(
+          padding: const EdgeInsets.only(left: 10),
+          child: Image.asset("lib/img/zdj1.png"),
         ),
         backgroundColor: Color.fromARGB(255, 20, 22, 26),
       ),
