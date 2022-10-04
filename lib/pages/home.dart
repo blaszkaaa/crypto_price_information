998c9d8f4d6acbd6137a3147bbc8089f72068e1c
blaszkaaa <58654924+blaszkaaa@users.noreply.github.com>
2022-10-04 12:32:27 +0200

    1

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors, camel_case_types
+
 import 'package:flutter/material.dart';
 class homeUser extends StatelessWidget {
   const homeUser({Key? key}) : super(key: key);
@@ -6,7 +8,10 @@ class homeUser extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Crypto Price"),
+        title: Center(
+          child: Text("Crypto Price"),
+        ),
+        backgroundColor: Color.fromARGB(255, 20, 22, 26),
       ),
       body: Column(
 
