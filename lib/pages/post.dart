ea5c6f3ec389cd6e25be28ef9383f8ef9dc84ba5
blaszkaaa <58654924+blaszkaaa@users.noreply.github.com>
2022-10-04 17:46:38 +0200

    1

--- lib/pages/post.dart
+++ lib/pages/post.dart
@@ -38,6 +38,7 @@ class _postCryptoState extends State<postCrypto> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Color.fromARGB(255, 11, 12, 14),
         body: ListView.builder(
           itemCount: _postsJson.length,
           itemBuilder: (context, i) {
@@ -49,16 +50,20 @@ class _postCryptoState extends State<postCrypto> {
                 width: 200,
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(12),
-                  color: Colors.black54,
+                  color: Color.fromARGB(255, 20, 22, 26),
                 ),
-                child: Column(
+                child: Row(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                   //zdjęcie
-                    ClipRRect(
-                      borderRadius: BorderRadius.circular(12),
-                      child: Image.network("${post["image"]}", width: 50,)
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 8),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(12),
+                        child: Image.network("${post["image"]}", width: 50,)
+                      ),
                     ),
+                  // nazwa
                   Padding(
                     padding: const EdgeInsets.symmetric(vertical: 12.0, horizontal: 8),
                     child: Column(
@@ -66,12 +71,23 @@ class _postCryptoState extends State<postCrypto> {
                       children: [
                         Text(
                         "${post["name"]}",
-                        style: TextStyle(fontSize: 20),  
+                        style: TextStyle(
+                          fontSize: 24,
+                          color: Colors.white,
+                          ),  
+                        ),
+                        Text("Price ${post["current_price"]} " + "\$", 
+                        style: TextStyle(
+                          color: Colors.grey[500]
                           ),
-                        SizedBox(height: 4,),
-                    ],
+                        ),
+                      ],
+                    ),
                   ),
-                ),],
+                ],
+
+
+
               ),
             ),
           );}
